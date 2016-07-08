d3324d4b80c36cf730d134aec6eeef02260c63aa
v-xucxia <v-xucxia@PC-ELVISXIA>
2016-07-08 14:28:11 +0800

    Add project files.

---
+++ WinRTXamlToolkitSample/MainPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WinRTXamlToolkit.Sample.Common;
+using WinRTXamlToolkit.Tools;
+using WinRTXamlToolkit.Imaging;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WinRTXamlToolkitSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private int _branchId;
+        private Random _rand = new Random();
+        private List<Color> _namedColors = ColorExtensions.GetNamedColors();
+
+        #region TreeItems
+        private ObservableCollection<TreeItemViewModel> _treeItems;
+        public ObservableCollection<TreeItemViewModel> TreeItems
+        {
+            get { return _treeItems; }
+            set { this._treeItems = value; }
+        }
+        #endregion
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            TreeItems = BuildTree(10, 10);
+            myTreeView.ItemsSource = TreeItems;
+            //myListView.ItemsSource = TreeItems;
+        }
+
+        private ObservableCollection<TreeItemViewModel> BuildTree(int depth, int branches)
+        {
+            var tree = new ObservableCollection<TreeItemViewModel>();
+
+            if (depth > 0)
+            {
+                var depthIndices = Enumerable.Range(0, branches).Shuffle();
+
+                for (int i = 0; i < branches; i++)
+                {
+                    var d = depthIndices[i] % depth;
+                    var b = _rand.Next(branches / 2, branches);
+
+                    tree.Add(
+                        new TreeItemViewModel
+                        {
+                            Text = "Branch " + _branchId++,
+                            Brush = new SolidColorBrush(_namedColors[_rand.Next(0, _namedColors.Count)]),
+                            Children = BuildTree(d, b)
+                        });
+                }
+            }
+
+            return tree;
+        }
+    }
+
+
+    public class TreeItemViewModel : BindableBase
+    {
+        #region Text
+        private string _text;
+        /// <summary>
+        /// Gets or sets the text.
+        /// </summary>
+        public string Text
+        {
+            get { return _text; }
+            set { this.SetProperty(ref _text, value); }
+        }
+        #endregion
+
+        #region Children
+        private ObservableCollection<TreeItemViewModel> _children = new ObservableCollection<TreeItemViewModel>();
+        /// <summary>
+        /// Gets or sets the child items.
+        /// </summary>
+        public ObservableCollection<TreeItemViewModel> Children
+        {
+            get { return _children; }
+            set { this.SetProperty(ref _children, value); }
+        }
+        #endregion
+
+        #region Brush
+        private SolidColorBrush _brush;
+        /// <summary>
+        /// Gets or sets the brush.
+        /// </summary>
+        public SolidColorBrush Brush
+        {
+            get { return _brush; }
+            set { this.SetProperty(ref _brush, value); }
+        }
+        #endregion
+    }
+}
---
+++ WinRTXamlToolkitSample/TreeViewItemStyleSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WinRTXamlToolkitSample
+{
+    public class TreeViewItemStyleSelector:StyleSelector
+    {
+        public TreeViewItemStyleSelector()
+        {
+            var abc= 123;
+        }
+        public Style ResourceStyle { get; set; }
+
+        public Style ClassroomStyle { get; set; }
+
+        protected override Style SelectStyleCore(object item, DependencyObject container)
+        {
+            return base.SelectStyleCore(item, container);
+        }
+        
+    }
+}
